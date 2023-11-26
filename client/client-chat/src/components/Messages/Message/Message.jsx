9309436656144e6be827a16e1de4c531fc5fa80a
Jason Felix <jasonfelix789@gmail.com>
2023-11-25 22:25:01 -0700

    Styling updates

--- client/client-chat/src/components/Messages/Message/Message.jsx
+++ client/client-chat/src/components/Messages/Message/Message.jsx
@@ -26,7 +26,7 @@ const Message = ({ message: { text, user }, name }) => {
             : (
                 <div className="messageContainer justifyStart">
                     <div className="messageBox backgroundLight">
-                        <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
+                        <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
                     </div>
                     <p className="sentText pl-10 ">{user}</p>
                 </div>
