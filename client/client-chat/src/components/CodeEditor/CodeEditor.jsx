022f92e7eddcd811689c5c6e68f57e12dfbbf0c9
Jason Felix <jasonfelix789@gmail.com>
2023-12-13 10:44:09 +0530

    read mode

--- client/client-chat/src/components/CodeEditor/CodeEditor.jsx
+++ client/client-chat/src/components/CodeEditor/CodeEditor.jsx
@@ -3,6 +3,7 @@ import CodeMirror from '@uiw/react-codemirror';
 import { python } from '@codemirror/lang-python';
 import { javascript } from '@codemirror/lang-javascript';
 import { basicLight } from '@uiw/codemirror-theme-basic';
+import { dracula } from '@uiw/codemirror-theme-dracula';
 import { play } from '../../assets';
 import axios from 'axios';
 
@@ -125,86 +126,10 @@ const CodeEditor = ({ socket }) => {
     }
   }, [socket]);
 
-//   useEffect(() => {
-//     // Authenticate on component mount
-//     authenticate();
-//   }, []);
-
-/*return (
-    <div className='grid grid-rows-5 gap-4 h-full w-full box-border'>
-      <div className='row-span-4 flex flex-col w-full h-full overflow-auto'>
-        <div style={{ maxWidth: '100%', width: '100%' }}>
-          <CodeMirror
-            value={value}
-            height='400px'
-            width='100%'
-            theme={basicLight}
-            extensions={languageSelection}
-            onChange={onChangeCode}
-          />
-        </div>
-        <div className='col-span-5 flex p-2 justify-between bg-slate-200'>
-          <div className='col-span-1'>
-            <select
-              className='bg-gray-400 hover:bg-gray-500 rounded-lg p-4'
-              value={selectedLanguage}
-              onChange={handleLanguageChange}
-            >
-              <option value='python'>Python</option>
-              <option value='javascript'>JavaScript</option>
-            </select>
-          </div>
-          {userHasControl ? (
-            <button onClick={releaseControl} className='col-span-1'>
-              Release control
-            </button>
-          ) : null}
-          {controlUser === 'Nobody' ? (
-            <button onClick={takeControl} className='col-span-1'>
-              Take control
-            </button>
-          ) : null}
-          <p>{userHasControl ? 'You have control' : `${controlUser} has control`}</p>
-          <button className='col-span-1 bg-gray-400 hover:bg-gray-500' onClick={handleSave}>
-            Save
-          </button>
-          <button onClick={executeCode}>
-            <img src={play} className='w-4' alt='Run' />
-          </button>
-        </div>
-      </div>
-      <div className='row-span-1 p-4 flex flex-col items-center justify-center bg-black'>
-        {isPlayButtonPressed ? null : <h1 className='text-gray-600'>Output</h1>}
-        <div className='text-gray-400'>{output}</div>
-      </div>
-    </div>
-  );
-};*/
-
-// /**return (
-//     <div className='grid grid-rows-5 gap-4 h-full w-full box-border'>
-//       <div className='row-span-4 flex flex-col w-full h-full overflow-auto'>
-//         <div style={{ maxWidth: '100%', width: '100%' }}>
-//           {/* CodeMirror and other UI elements */}
-//         /*</div>
-//         <div className='col-span-5 flex p-2 justify-between bg-slate-200'>
-//           {/* Language selection and control buttons */}
-//         </div>
-//       </div>
-//       <div className='row-span-1 p-4 flex flex-col items-center justify-center bg-black'>
-//         <h1 className='text-gray-600'>Output</h1>
-//         <div className='text-gray-400'>{output}</div>
-//       </div>
-//       {isPlayButtonPressed && (
-//         <div className='absolute inset-0 flex items-center justify-center'>
-//           <div className='bg-white p-4 rounded'>
-//             <p className='text-gray-600'>Executing code...</p>
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };**/
+  let statusBarClass = `col-span-5 flex p-6 justify-between gap-3 bg-neutral-900 text-gray-400`
+    if (userHasControl) {
+        statusBarClass = `col-span-5 flex p-2 justify-between bg-slate-200 text-gray-600`
+    }
 
 return (
     <div className='grid grid-rows-5 gap-4 h-full w-full box-border'>
@@ -214,36 +139,35 @@ return (
             value={value}
             height='400px'
             width='100%'
-            theme={basicLight}
+            theme={userHasControl ? basicLight : dracula}
+            readOnly={!userHasControl}
             extensions={languageSelection}
             onChange={onChangeCode}
           />
         </div>
-        <div className='col-span-5 flex p-2 justify-between bg-slate-200'>
-          <div className='col-span-1'>
-            <select
-              className='bg-gray-400 hover:bg-gray-500 rounded-lg p-4'
-              value={selectedLanguage}
-              onChange={handleLanguageChange}
-            >
-              <option value='python'>Python</option>
-              <option value='javascript'>JavaScript</option>
-            </select>
-          </div>
-          {userHasControl ? (
-            <button onClick={releaseControl} className='col-span-1'>
-              Release control
-            </button>
-          ) : null}
+        <div className={statusBarClass}>
+        {userHasControl ?
+            <div className='col-span-1'>
+                {/* Dropdown button */}
+                <select
+                    className='bg-gray-400 hover:bg-gray-500 rounded-lg p-4'
+                    value={selectedLanguage}
+                    onChange={handleLanguageChange}
+                >
+                    <option value='python'>Python</option>
+                    <option value='javascript'>JavaScript</option>
+                </select>
+            </div> : null}
+              {userHasControl ? (
+              <button onClick={releaseControl} className='col-span-1 bg-gray-400 hover:bg-gray-500'>Release control</button>) 
+              : null}
           {controlUser === 'Nobody' ? (
-            <button onClick={takeControl} className='col-span-1'>
-              Take control
-            </button>
-          ) : null}
+              <button onClick={takeControl} className='col-span-1 bg-neutral-800 hover:bg-neautral-900 text-gray-400'>Take control</button>)
+           : null}
           <p>{userHasControl ? 'You have control' : `${controlUser} has control`}</p>
-          <button className='col-span-1 bg-gray-400 hover:bg-gray-500' onClick={handleSave}>
+          {userHasControl? (<button className='col-span-1 bg-gray-400 hover:bg-gray-500' onClick={handleSave}>
             Save
-          </button>
+          </button>) : null}
           <button onClick={executeCode}>
             <img src={play} className='w-4' alt='Run' />
           </button>
