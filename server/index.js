f6420dc2bd9d441d4fc8d73ffca5a754d4f68057
vedant29 <vedantk29@gmail.com>
2023-12-12 20:42:08 -0700

    GCP configured

--- server/index.js
+++ server/index.js
@@ -4,14 +4,24 @@ const { Server } = require('socket.io');
 const http = require('http');
 const cors = require('cors'); // Import cors module
 const mongoose = require('mongoose');
-const { createProxyMiddleware } = require('http-proxy-middleware'); // Import the middleware
+const { Storage } = require('@google-cloud/storage');
+const multer = require('multer');
+const upload = multer();
+const fs = require('fs');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const serviceAccount = require('./Credentials.json');
+const path = require('path');
+const os = require('os');
+const bodyParser = require('body-parser');
 
 
 //Custom imports
 const Room = require('./models/Room');
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 const router = require('./router');
-
+const credentials = JSON.parse(fs.readFileSync('./Credentials.json'));
+const pdf = require('html-pdf');
 
 //port 
 const PORT = process.env.PORT || 5000;
@@ -20,25 +30,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
 
-/*app.use(function (request, response, next) {
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });*/
-app.use(
-    '/v1',
-    createProxyMiddleware({
-        target: 'https://api.jdoodle.com',
-        changeOrigin: true,
-        pathRewrite: {
-            '^/v1': '/v1',
-  },
-  onProxyRes: function (proxyRes, req, res) {
-    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-    proxyRes.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
-        },
-    })
-);
+app.use(bodyParser.json());
 
 //connect to mongoDB
 mongoose.connect('mongodb://localhost:27017/Rooms', {
@@ -51,17 +43,6 @@ mongoose.connection.on('connected', () => {
     console.log('Connected to MongoDB');
 });
 
-/*app.use(
-    '/v1',
-    createProxyMiddleware({
-        target: 'https://api.jdoodle.com',
-        changeOrigin: true,
-        pathRewrite: {
-            '^/v1': '',
-        },
-    })
-
-);*/
 
 const io = new Server(server, {
     cors: {
@@ -70,7 +51,88 @@ const io = new Server(server, {
     }
 });
 
+const storage = new Storage({
+    projectId: 'credentials.project_id',
+    keyFilename: './Credentials.json',
+    bucket: 'screenshot_canvas',
+  });
+  //const bucketName = 'screenshot_canvas';
 
+  const multerStorage = multer.memoryStorage();
+
+app.post('/uploadCanvasScreenshot', upload.single('image'), async (req, res) => {
+    try {
+        const imageData = req.file?.buffer;
+        if (!imageData) {
+            throw new Error('No image data');
+        }
+
+      const fileName = `canvas_screenshot_${Date.now()}.png`;
+  
+      const bucket = storage.bucket('screenshots_canvas');
+      const file = bucket.file(fileName);
+  
+      await file.save(imageData, { contentType: 'image/png' });
+  
+      console.log(`Image uploaded to Google Cloud Storage: ${fileName}`);
+     
+      res.status(200).json({ success: true, fileName });
+    } catch (error) {
+      console.error('Error uploading image to Google Cloud Storage:', error);
+      res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+  });
+
+  app.get('/downloadCanvasImage/:fileName', async (req, res) => {
+    const { fileName } = req.params;
+    const bucket = storage.bucket('screenshots_canvas');
+    const file = bucket.file(fileName);
+    const [fileExists] = await file.exists();
+
+    if(fileExists) {
+        const readStream = file.createReadStream();
+        res.setHeader('Content-Type', 'image/png');
+        readStream.pipe(res);
+    }
+    else {
+        res.status(404).json({ success: false, error: 'File not found' });
+    }
+});  
+
+app.post('/saveCodeAsPDF', async (req, res) => {
+    try {
+      const { code } = req.body;
+  
+      const html = `<html><body><pre>${code}</pre></body></html>`;
+      const pdfOptions = { format: 'Letter' };
+  
+      pdf.create(html, pdfOptions).toBuffer(async (err, buffer) => {
+        if (err) {
+          console.error('Error creating PDF:', err);
+          res.status(500).json({ success: false, error: 'Internal Server Error' });
+          return;
+        }
+  
+        // Upload the PDF to the Google Cloud Storage bucket
+        const fileName = `code_${Date.now()}.pdf`;
+        const bucket = storage.bucket('screenshots_canvas');
+        const file = bucket.file(fileName);
+        await file.save(buffer);
+  
+        console.log(`PDF saved to Google Cloud Storage: ${fileName}`);
+  
+        // Send the PDF as a response
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', `attachment; filename=${fileName}`);
+        res.status(200).send(buffer);
+      });
+    } catch (error) {
+      console.error('Error saving code as PDF:', error);
+      res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+  });
+
+  
 io.on('connection', (socket) => {
     console.log(`We have a new connection ${socket.id}`);
 
